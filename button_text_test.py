2e2cc997e57e28ea29976d73dcbdc4b4faf6c5bf
Mike Donovan <mike.donovan@killeenisd.org>
2018-07-20 22:40:01 -0500

    add button test

---
+++ button_text_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from Tkinter import *
+class GraphicsInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry("720x500")
+
+        self.clicked=[]
+        button1 = Button(self.window, text="Dice 1", width=13)
+        button2 = Button(self.window, text="Dice 2", width=13)
+        button1.pack()
+        button2.pack()
+
+        button1.configure(command=lambda btn=button1: self.OnClick(btn))
+        button2.configure(command=lambda btn=button2: self.OnClick(btn))
+
+        self.window.mainloop()
+
+    def OnClick(self, btn):
+        text = btn.cget("text")
+        self.clicked.append(text)
+        print "clicked:", self.clicked
+
+app = GraphicsInterface()
